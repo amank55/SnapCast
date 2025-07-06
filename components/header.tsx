4e7b6359a2b403d34859a6e606713c71239ef5b3
amank55 <amankumarr2005ak@gmail.com>
2025-07-06 21:14:35 +0530

    Refactor Header component and remove background color from global styles. Enhanced Header with user image, upload link, and search functionality, while cleaning up unused styles in globals.css.

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,54 @@
 import React from 'react'
-
-const Header = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+import { ICONS } from '@/constants'
+const Header = ({subHeader,title,userImg}: SharedHeaderProps) => {
   return (
-    <div>Header</div>
+    <header className='header'>
+        <section className='header-container'>
+            <div className='details'>
+            {userImg &&(
+                <Image src={userImg ||'/assets/images/dummy.jpg'} alt ="user" width = {66} height = {66} className="rounded-full"/>
+            )}
+            </div>
+
+
+            <article>
+                <p>{subHeader}</p>
+                <h1>{title}</h1>
+            </article>
+
+            <aside>
+                <Link href = "/upload">
+                <Image src = "/assets/icons/upload.svg" alt = "upload" height= {16} width = {16}/>
+                <span>Upload a video</span>
+                </Link>
+
+                <div className='record'>
+                <button className="primary-btn bg-[#ff4393] hover:bg-pink-600">
+                    <Image src={ICONS.record} alt="camera" width={16} height={16}/>
+                    <span>Record a video</span>
+                </button>
+                </div>
+            </aside>
+        </section>
+
+        <section className="search-filter">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search for videos, tags, folders..."
+          />
+          <Image
+            src="/assets/icons/search.svg"
+            alt="search"
+            width={16}
+            height={16}
+          />
+        </div>
+        {`<DropdownList\>`}
+      </section>
+    </header>
   )
 }
 
