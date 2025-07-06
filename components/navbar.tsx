b08c2be0ee6a66e80aa78815e9e3c698f0141be6
amank55 <amankumarr2005ak@gmail.com>
2025-07-06 14:25:05 +0530

    Update layout and global styles. Changed font to Karla, added background color to body, and removed the unused page component.

---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='navbar'>
+        <nav>
+            <Link href={'/'}>
+            <Image src ="/assets/icons/logo.svg" alt="logo" width={32} height={32}/>
+            <h1>SnapCast</h1>
+            </Link>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
