a08486ff1466fcc3ffa704aad976d54ff398893e
amank55 <amankumarr2005ak@gmail.com>
2025-07-09 10:09:44 +0530

    Refactor main and profile pages to include VideoCard components within a section element for improved layout. Updated the main page and user profile to dynamically render video cards from the dummyCards array, enhancing content organization.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,10 +6,12 @@ import { dummyCards } from '@/constants';
 const page = () => {
   return (
     <main className="max-w-[1440px] w-full px-4 sm:px-6 lg:px-8 flex flex-col min-h-screen pt-12.5 pb-20 gap-9">
-      <Header title="All videos" subHeader="Public Library"/>    
-      {dummyCards.map(card => (
-        <VideoCard key={card.id} {...card} />
-      ))}
+      <Header title="All videos" subHeader="Public Library"/>
+      <section className="video-grid">
+        {dummyCards.map(card => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
       </main>
   )
 }
--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from '@/components/header'
+import { dummyCards } from '@/constants'
+import VideoCard from '@/components/VideoCard'
 const Page = async({params}:ParamsWithSearch) => {
     const {id} = await params
   return (
     <div className='wrapper page'>
         <Header subHeader="amankwork2005@gmail.com" title = "Aman Kumar" userImg= "/assets/images/dummy.jpg"/>
-       <h1 className='text-2xl font-karla'> USER ID: {id}</h1>
+        <section className="video-grid">
+        {dummyCards.map(card => (
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </div>
   )
 }
