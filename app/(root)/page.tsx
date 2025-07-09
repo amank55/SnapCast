d4674f1cd7b5963c07d040b60cff64a378af3157
amank55 <amankumarr2005ak@gmail.com>
2025-07-09 10:07:36 +0530

    Add dummy video data and update main page to render VideoCard components dynamically. Removed hardcoded VideoCard instances and integrated a mapping function to display video cards from the new dummyCards array.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,15 @@
 import Header from '@/components/header'
 import VideoCard from '@/components/VideoCard'
 import React from 'react'
+import { dummyCards } from '@/constants';
 
 const page = () => {
   return (
     <main className="max-w-[1440px] w-full px-4 sm:px-6 lg:px-8 flex flex-col min-h-screen pt-12.5 pb-20 gap-9">
-      <Header title="All videos" subHeader="Public Library"/>
-      <h1 className='text-3xl font-karla underline'>Name aman Kumar       
-      </h1>
-
-      <VideoCard
-        _id="1"
-        title="SnapChat Message – 30 June 2025"
-        thumbnail="/assets/samples/thumbnail (1).png"
-        createdAt="2025-05-01 06:25:54.437"
-        userImg="/assets/images/jason.png"
-        username="Jason"
-        views={10}
-        visibility="public"
-        duration={156}
-      />
+      <Header title="All videos" subHeader="Public Library"/>    
+      {dummyCards.map(card => (
+        <VideoCard key={card.id} {...card} />
+      ))}
       </main>
   )
 }
